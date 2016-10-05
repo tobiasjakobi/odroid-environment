f95ad338bc466c14aede7c3486330f6bc9c27fc0
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-10-06 00:37:21 +0200

    script: check for display connection in rarch
    Also try to switch HDMI into full range mode, using
    the exynos_mixer_cfg tool.

--- local/bin/rarch_launch.sh
+++ local/bin/rarch_launch.sh
@@ -12,6 +12,12 @@ rarch_config_input
 # Don't run the emulator if there is no gamepad attached.
 rarch_check_gamepad || exit 1
 
+# Don't run the emulator if there is no display attached.
+rarch_check_display || exit 2
+
 rarch_content=$(rarch_select_content "${1}")
+
+# Switch HDMI to full range mode.
+$HOME/local/bin/exynos_mixer_cfg -r 1
 
 retroarch "${1}" "${rarch_content}"
